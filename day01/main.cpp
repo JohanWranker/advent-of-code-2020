9366ca700c6ac33ed16f3ce1f4994ab6cf89a5b1
Johan Wranker <johan@wranker.org>
2020-12-01 18:40:20 +0100

    day1

--- day01/main.cpp
+++ day01/main.cpp
@@ -2,20 +2,43 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-    std::ifstream file;
+    ifstream file;
     file.open("input");
-    std::string line;
+    string line;
+    std::vector<int> list = {};
     
     if (! file.good()){
+        cout << "file not found" << endl;
         return 1;
     }
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl;
+    while (getline(file, line)) {
         int data = stoi(line);
-        std::cout << data << std::endl;
-
+        list.push_back(data);
     }
-    return 0;
+    std::sort(list.begin(), list.end()); 
+    for (int i : list) {
+        for (int j : list) {
+            for (int k : list) {
+                cout << i+j+k <<endl;
+                if (i+j+k == 2020) {
+                    cout << i*j*k <<endl;
+                    cout << i <<endl;
+                    cout << j <<endl;
+                    cout << k <<endl;
+                    return 0;
+                }
+                if (i+j+k > 2020) {
+                    break;
+                }
+            }
+        }
+    }
+    cout << "fail" << endl;
+    
+    return 1;
 }
