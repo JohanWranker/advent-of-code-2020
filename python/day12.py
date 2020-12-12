92ca1481c39929a860a301a77a06288234ea6418
Johan Wranker <johan@wranker.org>
2020-12-13 00:02:35 +0100

    day12 cleaned

--- python/day12.py
+++ python/day12.py
@@ -13,30 +13,40 @@ rawtext = open(path).readlines()
 
 ship_pos = [0, 0]
 waypoint_rel_pos = [10, 1]
-dirction = 'E'
 
-directions = ['N','E','S','W']
+Right = [ [0,1],[-1,0]]
+Left =  [ [0,-1],[1,0]]
+
+directions = { 
+    'N' : [0,1],
+    'E' : [1,0],
+    'S' : [0,-1],
+    'W' : [-1,0],
+} 
 
 for line in rawtext:
     order = line[:1]
     value = int(line[1:])
-    if order == 'N':
-        waypoint_rel_pos[1] += value
-    if order == 'S':
-        waypoint_rel_pos[1] -= value
-    if order == 'E':
-        waypoint_rel_pos[0] += value
-    if order == 'W':
-        waypoint_rel_pos[0] -= value
-    if order in ['F', 'B']:
-        ship_pos[0] = ship_pos[0] + waypoint_rel_pos[0]*value
-        ship_pos[1] = ship_pos[1] + waypoint_rel_pos[1]*value
+    if order in directions:
+        waypoint_rel_pos[0] += value*directions[order][0]
+        waypoint_rel_pos[1] += value*directions[order][1]
+    if order == 'F':
+        ship_pos[0] += waypoint_rel_pos[0]*value
+        ship_pos[1] += waypoint_rel_pos[1]*value
     if order =='R':
         for i in range(int(value/90)):
-            waypoint_rel_pos = [waypoint_rel_pos[1], -waypoint_rel_pos[0]]
+            waypoint_rel_pos = [ 
+                waypoint_rel_pos[0] * Right[0][0] + waypoint_rel_pos[1] * Right[0][1], 
+                waypoint_rel_pos[0] * Right[1][0] + waypoint_rel_pos[1] * Right[1][1], 
+                ]
     if order =='L':
         for i in range(int(value/90)):
-            waypoint_rel_pos = [-waypoint_rel_pos[1], waypoint_rel_pos[0]]
-    print (f"'{order}'{value}'  {ship_pos} {dirction} [ {waypoint_rel_pos}]")
+            waypoint_rel_pos = [ 
+                waypoint_rel_pos[0] * Left[0][0] + waypoint_rel_pos[1] * Left[0][1], 
+                waypoint_rel_pos[0] * Left[1][0] + waypoint_rel_pos[1] * Left[1][1], 
+                ]
+    # print (f"'{order}'{value}'  {ship_pos} {dirction} [ {waypoint_rel_pos}]")
 print(f"{abs(ship_pos[0])+ abs(ship_pos[1])}")
+assert 29839 == abs(ship_pos[0])+ abs(ship_pos[1])
+
 
